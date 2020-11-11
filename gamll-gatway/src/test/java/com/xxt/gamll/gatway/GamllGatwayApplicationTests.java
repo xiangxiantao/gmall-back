6d3765c00baed5db526f93aa0c4a5392bec84dba
xxt <1067301216@qq.com>
2020-11-12 00:36:33 +0800

    feat: gateway

---
+++ gamll-gatway/src/test/java/com/xxt/gamll/gatway/GamllGatwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxt.gamll.gatway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamllGatwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
